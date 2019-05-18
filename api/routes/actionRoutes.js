c7968272b34b3aac2dac439e20ec330e35d4f32b
LD <LDinho@users.noreply.github.com>
2019-05-18 14:49:52 -0400

    [MVP complete] add update & delete request endpoints for actions

--- api/routes/actionRoutes.js
+++ api/routes/actionRoutes.js
@@ -38,4 +38,64 @@ router.get('/:id', (req, res) => {
     })
 });
 
+// Update
+router.put('/:id', (req, res) => {
+
+  const { id } = req.params;
+  const updatedAction = req.body;
+
+  const { description } = updatedAction;
+
+  if (!description) {
+    return res.status(400)
+      .json({ errorMessage: "Description missing."});
+  }
+
+  update(id, updatedAction)
+    .then((action) => {
+      console.log("Action UPDATE::", action);
+
+      if (action) {
+        return res.status(200).json(action);
+      } else {
+        return res.status(404)
+          .json({
+            message: "Action with the specified ID does not exist."
+          });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "Server error."
+      });
+    })
+
+});
+
+// Delete
+router.delete('/:id', (req, res) => {
+
+  const { id } = req.params
+
+  get(id)
+    .then((action) => {
+      if (action) {
+        console.log("Action in DELETE", action);
+
+        remove(id).then(()=> {
+          res.status(200).json(action);
+        }).catch(() => {
+          res.status(500).json({ error: "Bad Server-Unable to remove" });
+        })
+      } else {
+        res.status(404)
+          .json({ message: "Project with the specified ID does not exist." });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Server error" });
+    })
+
+});
+
 module.exports = router
