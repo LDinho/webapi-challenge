093b62ddd4ffd74d015e2035faf9c5a59c781aaf
LD <LDinho@users.noreply.github.com>
2019-05-18 14:24:21 -0400

    add get request endpoint for all actions

--- api/routes/actionRoutes.js
+++ api/routes/actionRoutes.js
@@ -9,5 +9,15 @@ const {
 } = require('../../data/helpers/actionModel');
 
 
+// Read all actions
+router.get('/', (req, res) => {
+  get()
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Server error", err })
+    })
+})
 
 module.exports = router
