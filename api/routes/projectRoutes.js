3dc683c97a624b8d846fb952c71fa9add7cb122b
LD <LDinho@users.noreply.github.com>
2019-05-18 13:29:39 -0400

    add post request endpoint for new project; fix projectModel helper methods error

--- api/routes/projectRoutes.js
+++ api/routes/projectRoutes.js
@@ -10,6 +10,30 @@ const {
   getProjectActions,
 } = require('../../data/helpers/projectModel');
 
+// POST- Create new project
+router.post('/', (req, res) => {
+  const newProject = req.body;
+  console.log('req body', req.body);
+
+  const { name, description} = newProject;
+
+  if (!name || !description) {
+    return res.status(400)
+      .json({
+        errorMessage: "name/description missing."
+      });
+  }
+
+  insert(newProject)
+    .then((newProject) => {
+      res.status(201).json(newProject);
+    })
+    .catch(() => {
+      res.status(500)
+         .json({ error: "Server error" });
+    })
+});
+
 // GET all projects
 router.get('/', (req, res) => {
   get()
