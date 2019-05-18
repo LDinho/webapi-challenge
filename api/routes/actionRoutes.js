19c07c30a8aa307af528002452730ff911fc2596
LD <LDinho@users.noreply.github.com>
2019-05-18 14:27:27 -0400

    add get request for one action by id

--- api/routes/actionRoutes.js
+++ api/routes/actionRoutes.js
@@ -9,7 +9,7 @@ const {
 } = require('../../data/helpers/actionModel');
 
 
-// Read all actions
+// GET all actions
 router.get('/', (req, res) => {
   get()
     .then(actions => {
@@ -19,5 +19,23 @@ router.get('/', (req, res) => {
       res.status(500).json({ message: "Server error", err })
     })
 })
+
+// GET one action by id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  get(id)
+    .then(action => {
+      console.log('ACTION GET ID:', action)
+      if (!action) {
+        return res.status(400).json(`message: action id ${id} is invalid`)
+      } else {
+        return res.status(200).json(action)
+      }
+    })
+    .catch(() => {
+      return res.status(500).json({ message: "Server error" })
+    })
+});
 
 module.exports = router
