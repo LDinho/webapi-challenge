b43dcf1bf0770268c95db34743deea040a3d96d3
LD <LDinho@users.noreply.github.com>
2019-05-18 13:16:11 -0400

    add get request endpoint for all projects

--- api/routes/projectRoutes.js
+++ api/routes/projectRoutes.js
@@ -10,5 +10,17 @@ const {
   getProjectActions,
 } = require('../../data/helpers/projectModel');
 
+// GET all projects
+router.get('/', (req, res) => {
+  get()
+    .then(projects => {
+      res.status(200).json(projects)
+    })
+    .catch(err => {
+      res.status(500)
+         .json({ message: "Server error", err })
+    })
+})
+
 
 module.exports = router
